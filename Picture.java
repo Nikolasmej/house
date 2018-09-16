bf57a5cf0e5cfc8f5ce872ca5e182669f05c61ca
Nicolas M <43154687+Nikolasmej@users.noreply.github.com>
2018-09-16 18:19:23 -0400

    background color
    changed background color = black

--- Picture.java
+++ Picture.java
@@ -11,17 +11,21 @@
  */
 public class Picture
 {
+    private Square road;
     private Square wall;
     private Square window;
     private Square carWindow;
     private Triangle roof;
     private Triangle pyramid;
     private Circle sun;
+    private Circle halfMoon;
     private Circle wheel1;
     private Circle wheel2;
     private Circle wheel3;
     private Circle wheel4;
     private Person me;
+    private Person me2;
+    private Person me3;
     private boolean drawn;
 
     /**
@@ -29,13 +33,17 @@ public class Picture
      */
     public Picture()
     {
+        road = new Square();
         wall = new Square();
         me = new Person();
+        me2 = new Person();
+        me3 = new Person();
         window = new Square();
         carWindow = new Square();
         roof = new Triangle();
         pyramid = new Triangle();
         sun = new Circle();
+        halfMoon = new Circle();
         wheel1 = new Circle();
         wheel2 = new Circle();
         wheel3 = new Circle();
@@ -46,10 +54,17 @@ public class Picture
 
     /**
      * Draw this picture.
+     * Methods
      */
     public void draw()
     {
         if(!drawn) {
+            road.moveHorizontal(-310);
+            road.moveVertical(10);
+            road.changeSize(500);
+            road.changeColor("green");
+            road.makeVisible();
+            
             wall.moveHorizontal(100);
             wall.moveVertical(20);
             wall.changeSize(55);
@@ -62,29 +77,35 @@ public class Picture
             carWindow.changeSize(20);
             carWindow.makeVisible();
             
-            window.changeColor("green");
+            window.changeColor("red");
             window.moveHorizontal(-120);
             window.moveVertical(40);
             window.changeSize(40);
             window.makeVisible();
             
             roof.changeColor("yellow");
-            roof.changeSize(100, 180);
-            roof.moveHorizontal(90);
+            roof.changeSize(100, 150);
+            roof.moveHorizontal(75);
             roof.moveVertical(-40);
             roof.makeVisible();
             
             pyramid.changeColor("yellow");
-            pyramid.changeSize(120, 200);
+            pyramid.changeSize(160, 200);
             pyramid.moveHorizontal(-100);
-            pyramid.moveVertical(-60);
+            pyramid.moveVertical(-100);
             pyramid.makeVisible();
             
-            sun.changeColor("red");
+            sun.changeColor("white");
             sun.moveHorizontal(120);
             sun.moveVertical(-80);
-            sun.changeSize(80);
+            sun.changeSize(60);
             sun.makeVisible();
+            
+            halfMoon.changeColor("black");
+            halfMoon.moveHorizontal(100);
+            halfMoon.moveVertical(-80);
+            halfMoon.changeSize(60);
+            halfMoon.makeVisible();
             
             wheel1.changeColor("black");
             wheel1.moveHorizontal(180);
@@ -113,6 +134,16 @@ public class Picture
             me.makeVisible();
             me.moveVertical(40);
             me.moveHorizontal(-120);
+            
+            me2.makeVisible();
+            me2.changeSize(20,20);
+            me2.moveVertical(-20);
+            me2.moveHorizontal(-70);
+            
+            me3.makeVisible();
+            me3.moveVertical(-25);
+            me3.changeSize(40,30);
+            me3.moveHorizontal(110);
             
             drawn = true;
         }
